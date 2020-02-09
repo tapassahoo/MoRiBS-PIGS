9a0ffe461b4db2b658a0280ca0205e9a40c03101
Tapas Sahoo <tapascuchem@gmail.com>
2020-02-08 19:30:39 -0500

    scripts for submission changed

--- examples/scripts/code-asymrho.py
+++ examples/scripts/code-asymrho.py
@@ -9,17 +9,15 @@ space = " "
 file_sim = module_path + "script_submission_asymrho.py"
 
 # Informations about the system
-simType = "S"
+simType = "A"
 rotor = "H2O"
 param = "tau"
 value = 0.002
 #param = "beta"
 #value = 0.2
-#nbeads = [4, 6, 8, 10]#, 14, 20]
-#nbeads = [14, 20, 24, 30, 40, 50]
-nbeads = [60, 70, 80, 90]
-#nbeads = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-#nbeads = [120, 140, 160, 180, 200]
+#nbeads = [4, 6, 8, 10, 14, 20, 24]
+#nbeads = [30, 40, 50, 60, 70, 80, 90, 100]
+nbeads = [120, 140, 160, 180, 200]
 #nbeads += [200, 250, 300, 350]
 #nbeads += [400]
 Jmax = 66
--- examples/scripts/code-nonlinear-moribs-ent.py
+++ examples/scripts/code-nonlinear-moribs-ent.py
@@ -10,26 +10,26 @@ module_path=module_path.replace('__init__.py', '')
 
 # Informations about the system
 simType = "ENT"
-#simType1="submission "
-simType1 = "analysis"
+simType1="submission "
+#simType1 = "analysis"
 
 molecule = "H2O"
 rotor = "H2O"
 SpinIsomer = 0
 
 var = "beta" # for fixed tau
-param = 0.005 # for fixed tau
+param = 0.002 # for fixed tau
 
 #var = "tau"  # for fixed beta
 #param = 0.2 # for fixed beta
 
 #field_strength = 20.0 # Unit inverse of Kelvin
 nMolecule = 2
-nblocks = 40000
-npass = 200
+nblocks = 20000
+npass = 500
 
 if simType1 == "analysis":
-	cmd1 = "--preskip 0"
+	cmd1 = "--preskip 20000"
 else:
 	cmd1 = ""
 
