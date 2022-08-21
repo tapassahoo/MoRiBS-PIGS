f97aaf9473521e91e3cdc6db217c52589eaaad3d
Tapas Sahoo <tapascuchem@gmail.com>
2022-08-21 20:53:38 +0530

    Darwin: Updated on 21-08-2022

--- examples/scripts/code-linear-moribs-pigs.py
+++ examples/scripts/code-linear-moribs-pigs.py
@@ -25,35 +25,35 @@ parameter_value = 0.2
 variable_name = "tau"
 
 if (job_type == "submission"):
-    rlist = np.arange(2.5, 10.1, 0.1, dtype=float)
+	rlist = np.arange(2.5, 10.1, 0.1, dtype=float)
 
 if (job_type == "analysis"):
-    if (variable_name == "beta"):
-        rlist = np.arange(3.0, 10.1, 1.0, dtype=float)
-    if (variable_name == "tau"):
-        if (parameter_value == 0.1):
-            rlist = np.arange(2.5, 10.1, 0.1, dtype=float)
-        if (parameter_value == 0.2):
-            rlist = np.arange(2.5, 10.1, 0.1, dtype=float)
+	if (variable_name == "beta"):
+		rlist = np.arange(3.0, 10.1, 1.0, dtype=float)
+	if (variable_name == "tau"):
+		if (parameter_value == 0.1):
+			rlist = np.arange(2.5, 10.1, 0.1, dtype=float)
+		if (parameter_value == 0.2):
+			rlist = np.arange(2.5, 10.1, 0.1, dtype=float)
 
 string_layer2 = '""'
 
 space = " "
 for rcom in rlist:
-    dir_layer1 = "linear-rotors"
-    path_file_name = module_path + "submission_analysis.py"
-    temp_file_name = "script_submission_analysis.py"
-    print(path_file_name)
-    print(temp_file_name)
-    support.replace(
-        "name_of_output_directory",
-        dir_layer1,
-        path_file_name,
-        temp_file_name)
+	dir_layer1 = "linear-rotors"
+	path_file_name = module_path + "submission_analysis.py"
+	temp_file_name = "script_submission_analysis.py"
+	print(path_file_name)
+	print(temp_file_name)
+	support.replace(
+		"name_of_output_directory",
+		dir_layer1,
+		path_file_name,
+		temp_file_name)
 
-    #file_name3 = "script_submission_analysis-" + string_layer1 + ".py"
-    #support.replace("extraName", stringName2, fileName2, fileName3)
-    #call(["rm", fileName2])
+	#file_name3 = "script_submission_analysis-" + string_layer1 + ".py"
+	#support.replace("extraName", stringName2, fileName2, fileName3)
+	#call(["rm", fileName2])
 #
 #	rcom="{:3.2f}".format(rcom)
 #	field_strength = 20.0 # Unit inverse of Kelvin
@@ -89,6 +89,6 @@ for rcom in rlist:
 #		#+ " -NR 20000"+space
 #	)
 
-    # print(cmd_run)
-    # os.system(cmd_run)
-    #call(["rm", file_name3])
+	# print(cmd_run)
+	# os.system(cmd_run)
+	#call(["rm", file_name3])
