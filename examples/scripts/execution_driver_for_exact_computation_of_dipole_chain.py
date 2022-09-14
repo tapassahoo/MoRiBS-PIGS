90ffe03bd3033ab18d5d2dedd81ddc6f2fd97154
Tapas Sahoo <tapascuchem@gmail.com>
2022-09-14 11:27:46 +0530

    Linux: Updated on 14-09-2022

--- examples/scripts/execution_driver_for_exact_computation_of_dipole_chain.py
+++ examples/scripts/execution_driver_for_exact_computation_of_dipole_chain.py
@@ -23,7 +23,7 @@ blank_space = " "
 # Informations about the system
 rotor_name="HF"
 numb_molecule=2
-rlist = np.arange(3.0, 10.01, 1.0, dtype=float)
+rlist = np.arange(3.0, 10.01, 0.2, dtype=float)
 dipole_moment = 1.827
 l_max=20
 l_total_max=l_max
