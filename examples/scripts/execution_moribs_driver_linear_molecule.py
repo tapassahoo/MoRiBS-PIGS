292f3db15dae9beeef243aa678873e28623d19dc
Tapas Sahoo <tapascuchem@gmail.com>
2022-09-19 12:39:22 +0530

    Linux: Updated on 19-09-2022

--- examples/scripts/execution_moribs_driver_linear_molecule.py
+++ examples/scripts/execution_moribs_driver_linear_molecule.py
@@ -10,19 +10,24 @@ module_path = mypkg.moribs_runner.__file__
 module_path = module_path.replace('__init__.py', '')
 
 # User needs to modify once
-dir_name = "linear-rotors"
+submission_root_dir_name = "linear-rotors"
 # /home/tapas/MoRiBS-PIGS/examples/scripts
+
+"""
 dir_moribs = '""'
-# /home/tapas/academic-project/MoRiBS-PIGS/examples/scripts
-#dir_moribs = '"academic-project/"'
-extra_name = '""'
-#plot_dir_path = '"academic-project/outputs/"'
 plot_dir_path = '""'
+"""
+
+# /home/tapas/academic-project/MoRiBS-PIGS/examples/scripts
+dir_moribs = '"academic-project/"'
+plot_dir_path = '"academic-project/outputs/"'
+
+extra_name = '""'
 blank_space = " "
 
 # job_type is two types - "submission" and "analysis"
-#job_type = "submission"
-job_type = "analysis"
+job_type = "submission"
+#job_type = "analysis"
 method = "PIGS"
 
 system = "HF"
@@ -33,7 +38,7 @@ parameter_name = "beta"
 parameter_value = 0.2
 
 numb_molecule = 4
-numb_block = 20000
+numb_block = 20
 numb_pass = 200
 numb_preskip = 0
 
@@ -43,7 +48,7 @@ elif (numb_molecule == 1):
 	dipole_moment = 20.0  # It refer to the field strength and the unit inverse of Kelvin
 
 if (job_type == "submission"):
-	rlist = np.arange(5.0, 10.01, 0.2, dtype=float)
+	rlist = np.arange(5.0, 10.01, 5.2, dtype=float)
 
 if (job_type == "analysis"):
 	if (parameter_name == "beta"):
@@ -54,13 +59,13 @@ if (job_type == "analysis"):
 
 # No need to change the below three lines
 original_file_name = module_path + "submission_analysis.py"
-temp_file_name = "script_submission_analysis-" + dir_name + "-temp.py"
-temp_file_name1 = "script_submission_analysis-" + dir_name + "-temp1.py"
+temp_file_name = "script_submission_analysis-" + submission_root_dir_name + "-temp.py"
+temp_file_name1 = "script_submission_analysis-" + submission_root_dir_name + "-temp1.py"
 
 for rcom in rlist:
 	support.replace(
 		"name_of_output_directory",
-		dir_name,
+		submission_root_dir_name,
 		original_file_name,
 		temp_file_name)
 
@@ -103,6 +108,8 @@ for rcom in rlist:
 		+ "--nblock" + blank_space + str(numb_block) + blank_space
 		+ "--npass" + blank_space + str(numb_pass) + blank_space
 		+ "--preskip" + blank_space + str(numb_preskip) + blank_space
+		+ "--restart" + blank_space
+		+ "--nblock_restart" + blank_space + str(40) + blank_space
 	)
 
 	print(cmd_run)
